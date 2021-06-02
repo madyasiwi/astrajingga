b7e102a56c9392a9cd9172d2f1bb0f27611dbe8e
Indra Madyasiwi W <madyasiwi@gmail.com>
2021-06-02 20:10:12 +0700

    feat: add menu for building windows standalone player

--- Assets/Scripts/Editor/Build/BuildTools.cs
+++ Assets/Scripts/Editor/Build/BuildTools.cs
@@ -1,5 +1,6 @@
-using System.IO;
+using System;
 using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 
@@ -10,13 +11,12 @@ namespace madyasiwi.astrajingga.build {
     public static class BuildTools {
 
 
-        [MenuItem("Build/Update Version")]
-        public static void Build() {
+        static bool GetGitDescription(out string name, string commit="HEAD") {
             DirectoryInfo projectDir = new DirectoryInfo(Path.GetDirectoryName(Application.dataPath));
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.WorkingDirectory = projectDir.FullName;
             startInfo.FileName = "git";
-            startInfo.Arguments = "describe --tags";
+            startInfo.Arguments = $"describe --tags {commit}";
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
             startInfo.RedirectStandardError = true;
@@ -26,12 +26,38 @@ namespace madyasiwi.astrajingga.build {
             process.WaitForExit();
             if (process.ExitCode == 0) {
                 string version = process.StandardOutput.ReadToEnd();
+                name = version;
+                return true;
+            } else {
+                UnityEngine.Debug.LogError(process.StandardError.ReadToEnd());
+                name = null;
+                return false;
+            }
+        }
+
+
+        [MenuItem("Build/Update Version")]
+        public static void UpdateVersionNumber() {
+            string version;
+            if (GetGitDescription(out version)) {
                 PlayerSettings.bundleVersion = version;
                 UnityEngine.Debug.Log($"Version set to: {version}");
             } else {
                 UnityEngine.Debug.LogError("Failed to determine version for git tags.");
-                UnityEngine.Debug.LogError(process.StandardError.ReadToEnd());
             }
+        }
+
+
+        [MenuItem("Build/Build Player")]
+        public static void BuildPlayer() {
+            string[] scenes = Array.ConvertAll<EditorBuildSettingsScene, string>(EditorBuildSettings.scenes, (s) => { return s.path; });
+            DirectoryInfo buildDir = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(Application.dataPath), "Build", $"{PlayerSettings.productName}.exe"));
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            options.targetGroup = BuildTargetGroup.Standalone;
+            options.target = BuildTarget.StandaloneWindows;
+            options.scenes = scenes;
+            options.locationPathName = buildDir.FullName;
+            BuildPipeline.BuildPlayer(options);
         }
     }
 }
